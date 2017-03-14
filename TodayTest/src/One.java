aea9553d0a6da96914c7182bbdefb9fe84453929
am21980 <706873638@qq.com>
2017-03-14 11:18:31 +0800

    changes log

---
+++ TodayTest/src/One.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class One {
+	public static void main(String[] args){
+
+		System.out.println("please input a number between 1 to 1000");
+
+		int a = 456;
+
+		for(int b=1;b<=10;b++){
+
+		Scanner sc=new Scanner(System.in);
+
+		int i = sc.nextInt();
+
+		if(i>a)
+		{System.out.println("big!smaller!");}
+
+		if(i<a)
+		{System.out.println("small!bigger!");}
+
+		if(i==a)
+		{System.out.println("bingo!");}
+                        }
+              }
+	}
